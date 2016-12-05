078af826e52968cd4978917876b2d1bd5fdb3541
Guy Shahine <guy@resoltz.com>
2016-12-05 11:45:36 -0800

    fix web.config

--- Website/Earn/Global.asax
+++ Website/Earn/Global.asax
@@ -1,5 +1,1 @@
 <%@ Application Codebehind="Global.asax.cs" Inherits="Earn.MvcApplication" Language="C#" %>
-<!--
-Copyright (c) Microsoft Corporation. All rights reserved. 
-Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
--->
--- Website/Earn/Global.asax.cs
+++ Website/Earn/Global.asax.cs
@@ -12,31 +12,31 @@ using Earn.App_Start;
 
 namespace Earn
 {
-    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=9394801
-    public class MvcApplication : System.Web.HttpApplication
+  // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+  // visit http://go.microsoft.com/?LinkId=9394801
+  public class MvcApplication : System.Web.HttpApplication
+  {
+    protected void Application_Start()
     {
-        protected void Application_Start()
-        {
-            AreaRegistration.RegisterAllAreas();
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
-            BundleTable.EnableOptimizations = true;
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            HandleSslCertValidation();
-            ServicePointManager.DefaultConnectionLimit = 48;
-            ServicePointManager.Expect100Continue = false;
-            ServicePointManager.UseNagleAlgorithm = false;
-        }
-
-        private void HandleSslCertValidation()
-        {
-            ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
-        }
-
-        void Application_End(object sender, EventArgs e)
-        {
-        }
+      AreaRegistration.RegisterAllAreas();
+      BundleConfig.RegisterBundles(BundleTable.Bundles);
+      BundleTable.EnableOptimizations = true;
+      WebApiConfig.Register(GlobalConfiguration.Configuration);
+      FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+      RouteConfig.RegisterRoutes(RouteTable.Routes);
+      HandleSslCertValidation();
+      ServicePointManager.DefaultConnectionLimit = 48;
+      ServicePointManager.Expect100Continue = false;
+      ServicePointManager.UseNagleAlgorithm = false;
     }
+
+    private void HandleSslCertValidation()
+    {
+      ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
+    }
+
+    void Application_End(object sender, EventArgs e)
+    {
+    }
+  }
 }
